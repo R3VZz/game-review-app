d4d20f94c4383972e29fe9845cb0f14555e2e78e
daniloBraj <165780340+daniloBraj@users.noreply.github.com>
2024-06-05 12:05:07 +0100

    created createReview controller
    created controller to create new reviews.
    changed some of the delete controller.

--- back-end/controllers/controllers.js
+++ back-end/controllers/controllers.js
@@ -13,15 +13,22 @@ const editReview = async (req, res) => {
     res.status(200).json(review);
 }
 
+const createReview = async (req, res) => {
+    const {text} = req.body
+    console.log(text)
+    const reviewObject = new Review({
+        text
+    })
+
+    const newReview = await reviewObject.save()
+    res.status(200).json(newReview)
+}
+
 const deleteReview = async (req, res) => {
     // get id from ':id' param from the route
     const { id } = req.params
     // use mongoose model method findByIdAndDelete
     const deletedReview = await Review.findByIdAndDelete(id, updates)
-    // message if the review is not found
-    if (!review) {
-        return res.status(404).json( { message: "Item not found"} );
-    }
     // respond with a success message
     res.status(200).json(deletedReview)
 }
