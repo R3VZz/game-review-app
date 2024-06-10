3bfdf3cdb19da79a13ff2d9ffcca74c58aec78e0
daniloBraj <165780340+daniloBraj@users.noreply.github.com>
2024-06-10 14:21:06 +0100

    added css for home page

---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import './Home.css';
+
+const Home = () => {
+    const [gameData, setGameData] = useState([]);
+    const [err, setErr] = useState(null);
+
+    const fetchHandler = async () => {
+        try {
+            let response = await fetch("http://localhost:4000/api/games");
+    
+            if (!response) {
+                throw new Error(response.statusText)
+            }
+
+            let data = await response.json();
+            console.log(data);
+        
+            if (data.response && data.response.games) {
+            setGameData(data.response.games);
+            } else {
+            setGameData([]);
+            }
+
+        } catch (err) {
+            console.log(err.message);
+            setErr(err.message)
+        }
+    }
+
+    useEffect ( () => {
+        fetchHandler()
+    }, [])
+
+    return (
+        <div>
+            <h1>Game List</h1>
+            {err ? (<p>Error: {err}</p>) : (
+                <div className="game-info">
+                    {gameData.length > 0 ? (
+                        gameData.map((game) => (
+                            <div className="info" key={game.appid}>
+                                <p className="game-name">{game.name}</p>
+                            </div>
+                        ))
+                    ) : (<p>No games found</p>)
+                    }
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Home;
